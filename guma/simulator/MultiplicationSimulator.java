8adea6455f4f668ab05164ce149a14ee9b10ef78
Dimitrios Desyllas <ddesillas@yahoo.gr>
2013-03-20 14:30:50 +0200

    Adding Simulator for Multiplication

---
+++ guma/simulator/MultiplicationSimulator.java
@@ -0,0 +1,59 @@
+/**
+*GUMA a simple math game for elementary school students
+*	Copyright (C) 2012-2013  Dimitrios Desyllas (pc_magas)
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*Contact with me by main at thes address: pc_magas@yahoo.gr
+*/
+
+package guma.simulator;
+
+import guma.simulator.AbstractSimulator;
+import java.util.ArrayList;
+
+public class MultiplicationSimulator extends AbstractSimulator
+{
+
+	/**
+	*Array List that stores intermediate results
+	*/
+	private ArrayList<byte[]> intermediate=new ArrayList<byte[]>();
+
+	/**
+	*Constructor Method
+	*@param telestis1: the first operator of the number that we will simulate the first operation
+	*@param telestis2: the second operator of the number that we will simulate the second operation
+	*/
+	public MultiplicationSimulator(int telestis1,int telestis2)
+	{
+		super(telestis1,telestis2);
+	}
+
+	/**
+	*This Method does the next step of an arithmetic praxis Simulation
+	*Returns true if it has next step to do
+	*/	
+	public boolean next()
+	{
+		/*(telestis2Index>=0)
+		{
+			if(telestis1Index>=0)
+			{
+				
+			}
+		}*/
+		return true;
+	}
+		
+}
