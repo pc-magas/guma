53494bf3addd98e7af4562d30e71feeb8922aeb5
Dimitrios Desyllas <ddesillas@yahoo.gr>
2013-03-22 15:52:54 +0200

    Multiplication simulator fixes - halfway

--- guma/simulator/MultiplicationSimulator.java
+++ guma/simulator/MultiplicationSimulator.java
@@ -36,6 +36,10 @@ public class MultiplicationSimulator extends AbstractSimulator
 	private int endiamesaIndex=0;
 
 	/**
+	*Stores for each intermediate result where is the last digit that will participate into filan adding
+	*/
+	private int[] endiamesaLastDigit=null;
+	/**
 	*Shows what intermediate result will be chosen 
 	*/
 	private int endiamesoApotelesmaIndex=0;
@@ -48,17 +52,20 @@ public class MultiplicationSimulator extends AbstractSimulator
 	public MultiplicationSimulator(int telestis1,int telestis2)
 	{
 		super(telestis1,telestis2);
-		endiamesa=new byte[telestis2.length][];
-		
+		endiamesa=new byte[this.telestis2.length][];
+		endiamesaLastDigit=new int[this.telestis2.length];
+
 		for(int i=0;i<endiamesa.length;i++)
 		{
 			//From 
-			endiamesa[i]=new byte[telestis1.length+1];
+			endiamesa[i]=new byte[this.telestis1.length+1+i];
+			endiamesaLastDigit[i]=endiamesa[i].length-1;
 			Arrays.fill(endiamesa[i],(byte)0);
 		}
-		result=new byte[telestis1.length+telestis2.length];
+		result=new byte[this.telestis1.length+this.telestis2.length];
 		resultIndex=result.length-1;
 		endiamesaIndex=endiamesa[1].length-1;
+		
 	}
 
 	/**
@@ -67,39 +74,50 @@ public class MultiplicationSimulator extends AbstractSimulator
 	*/	
 	public boolean next()
 	{
+		message="";
 		if(telestis2Index>=0)
 		{
 			if(telestis1Index>=0)
 			{
 				message="Πολλαπλασιάζουμε τα ψηφία "+telestis1[telestis1Index]+"*"+telestis2[telestis2Index];
-				endiamesa[endiamesoApotelesmaIndex][endiamesaIndex]=telestis1[telestis1Index]*telestis2[telestis2Index];
+				temp=telestis1[telestis1Index]*telestis2[telestis2Index];
 				message+=" και ο πολαπλασιαμός έχει αποτέλεσμα: "+endiamesa[endiamesoApotelesmaIndex][endiamesaIndex];
 				if(kratoumeno!=0)
 				{
 					message+="\n Επειδή από την προηγούμενη πράξη έχουμε κρατούμενο το προσθέτοθμε στο αποτέλεσμα";					
-					endiamesa[endiamesoApotelesmaIndex][endiamesaIndex]+=kratoumeno
+					temp+=kratoumeno;
 				}
-				byte temp[]=AbstractSimulator.seperateDigits((int)endiamesa[endiamesoApotelesmaIndex][endiamesaIndex]);
-				if(temp.length>1)
+				byte tempM[]=AbstractSimulator.seperateDigits((int)endiamesa[endiamesoApotelesmaIndex][endiamesaIndex]);
+				if(tempM.length>1)
 				{
 					message="\n Η πράξη μας είχε διψήφιο αποτέλεσμα άρα το πρώτο ψηφίο το κρατάμε κρατούμενο";
-					kratoumeno=temp[0];
-					endiamesa[endiamesoApotelesmaIndex][endiamesaIndex]=temp[1];
+					kratoumeno=tempM[0];
+					endiamesa[endiamesoApotelesmaIndex][endiamesaIndex]=tempM[1];
 				}
 				else
 				{
 					kratoumeno=0;
+					endiamesa[endiamesoApotelesmaIndex][endiamesaIndex]=(byte)temp;
 				}
+				return true;
 			}
 			else
 			{
 				telestis2Index--;
+				telestis1Index=telestis1.length-1;
 				
+				if(telestis2Index>=0)
+				{
+					message="Βάζουμε ένα 0 κάτω από το "+endiamesa[resultIndex][endiamesa[resultIndex].length-1];
+					resultIndex++;
+					endiamesaLastDigit[resultIndex]=endiamesaLastDigit[resultIndex-1]-1;
+				}
+				return true;
 			}
 		}
 		else
 		{
-			//do adding between results
+
 		}
 	}
 		
