963d0b40641625e8b1e37ebfcf47e8fac4648f84
Dimitrios Desyllas <ddesillas@yahoo.gr>
2013-03-24 18:42:29 +0200

    Testing Multiplication Simulator Benchmark

--- guma/simulator/Benchmark.java
+++ guma/simulator/Benchmark.java
@@ -1,17 +1,25 @@
 package guma.simulator;
-import guma.simulator.AddingSimulator;
+import guma.simulator.*;
 public class Benchmark
 {
 	public static void main(String[] args)
 	{
-		AddingSimulator add=new AddingSimulator(100,200);
+		AbstractSimulator add=new MultiplicationSimulator(11,22);
 		boolean next=true;
 
 		do
 		{
+			try
+			{
 			next=add.next();
 			System.out.println(add.toString());
 			System.out.println("\t"+add.getTelestis1()+"\n\t"+add.getTelestis2()+"\n"+add.getResult());
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+				break;
+			}
 		}while (next);
 	}
 }
